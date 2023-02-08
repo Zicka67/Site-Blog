519866395f345da5b708989f324dda5a5d1a333d
Zicka67 <art.book@hotmail.fr>
2023-02-08 15:40:04 +0100

    Adding burger script

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById("background-video");
     video.play();
   });
+
+
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
