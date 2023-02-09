cc3698089abbab8bf12fb02922d2ba7f53c8fe4e
Zicka67 <art.book@hotmail.fr>
2023-02-09 11:40:17 +0100

    Trying to add JS for play button

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById("background-video");
     video.play();
   });
 
+  // ********************************************
 
 const burger = document.getElementById('burger');
 const navLinks = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
+    burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+
+    // *****************************************
+
+    const playButton = document.querySelector(".play-button");
+    const video = document.querySelector("video");
+
+playButton.addEventListener("click", function() {
+  video.play();
+});
 })
