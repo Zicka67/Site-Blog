71988e5e6dff017213da250a1e912980c0d31aec
Zicka67 <art.book@hotmail.fr>
2023-02-10 15:07:54 +0100

    Modification scrolltoTop

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   // ********************************************
 
     const burger = document.getElementById('burger');
-    const navLinks = document.querySelector('.left-side');
+    const navLinks = document.querySelector('left-side');
 
     burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
@@ -18,5 +18,24 @@
 
 playButton.addEventListener("click", function() {
   video.play();
+
+  // *********************************
+
+  // Get the button
+  const topBtn = $("#scrollTop"); // Target le bouton
+
+  //Faire aparaitre le bouton apres 40px
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 40) {
+      topBtn.show();
+    } else {
+      topBtn.hide();
+    }
+  });
+
+    // utilise la methode animate de JS Jquery pour remonter la page
+  topBtn.click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 2000);
+  });
 });
 })
