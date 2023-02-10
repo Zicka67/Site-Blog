25d341c3a41dae379dc05f1b5c108b79b3cf3bac
Zicka67 <art.book@hotmail.fr>
2023-02-10 16:29:54 +0100

    Modification for JS video play

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("background-video");
-    video.play();
-  });
-
-  // ********************************************
-
-    const burger = document.getElementById('burger');
-    const navLinks = document.querySelector('left-side');
-
-    burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-
-    // *****************************************
-
-    const playButton = document.querySelector(".play-button");
-    const video = document.querySelector("video");
-
-playButton.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  var video = document.getElementById("background-video");
   video.play();
+});
 
+// ********************************************
+
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('left-side');
+
+// burger.addEventListener('click', () => {
+//   navLinks.classList.toggle('active');})
+
+  // *****************************************
+
+  const playButton = document.querySelector(".play-button");
+  const video = document.getElementsByTagName("video");
+
+  playButton.addEventListener("click", function () {
+    video[1].play();
+    
+  })
   // *********************************
 
-  // Get the button
-  const topBtn = $("#scrollTop"); // Target le bouton
+//   // Get the button
+//   const topBtn = $("#scrollTop"); // Target le bouton
 
-  //Faire aparaitre le bouton apres 40px
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 40) {
-      topBtn.show();
-    } else {
-      topBtn.hide();
-    }
-  });
+//   //Faire aparaitre le bouton apres 40px
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 20) {
+//       topBtn.show();
+//     } else {
+//       topBtn.hide();
+//     }
+//   });
 
-    // utilise la methode animate de JS Jquery pour remonter la page
-  topBtn.click(function() {
-    $("html, body").animate({ scrollTop: 0 }, 2000);
-  });
-});
-})
+//     // utilise la methode animate de JS Jquery pour remonter la page
+//   topBtn.click(function() {
+//     $("html, body").animate({ scrollTop: 0 }, 2000);
+//   });
+// })
