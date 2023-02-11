46b1e5b5181dd400440d066d76e459020b543997
Zicka67 <art.book@hotmail.fr>
2023-02-12 00:03:53 +0100

    Adding + modif for sticky bar

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
+// ************ AUTO PLAY SUR L'ELEMENT background-video
+
 document.addEventListener("DOMContentLoaded", function () {
   var video = document.getElementById("background-video");
   video.play();
 });
 
-// ********************************************
+// ********************* MENU STICKY ***********************
 
-const burger = document.getElementById('burger');
-const navLinks = document.querySelector('left-side');
 
-// burger.addEventListener('click', () => {
-//   navLinks.classList.toggle('active');})
+// ********************* MENU BURGER ***********************
 
-  // ****************************************
+// const burgerMenu = document.querySelector('.burger-menu');
+// const dropdownMenu = document.querySelector('.dropdown-menu');
 
-  const playButton = document.querySelector(".play-button");
-  const video = document.querySelector("#video2");
-  
-  playButton.addEventListener("click", function() {
-    if (video.paused) {
-      video.play();
-      playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-    } else {
-      video.pause();
-      playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
-    }
-  });
-   
-  video.addEventListener("mouseover", function() { 
-    playButton.style.visibility = "visible";
-  });
-  
-  video.addEventListener("mouseout", function() {              
-    playButton.style.visibility = "hidden";
-  });
+// burgerMenu.addEventListener('click', () => {
+//   dropdownMenu.classList.toggle('show-dropdown');
+// });
+
+// ******************** PLAYBUTTON + PAUSE ********************
+
+const playButton = document.querySelector(".play-button");
+const video = document.querySelector("#video2");
+
+playButton.addEventListener("click", function() {
+  if (video.paused) {
+    video.play();
+    playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  } else {
+    video.pause();
+    playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+  }
+});
+
+video.addEventListener("mouseover", function() { 
+  playButton.style.visibility = "visible";
+});
+
+video.addEventListener("mouseout", function() {              
+  playButton.style.visibility = "hidden";
+});
 
 // ** transformer le button play on pause inerhtml + add class hover  
 //  + 
