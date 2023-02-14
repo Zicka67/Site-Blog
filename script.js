29dd3bbe4a42da243838334a99ead7ee6fca1768
Zicka67 <art.book@hotmail.fr>
2023-02-14 12:30:52 +0100

    Script pour afficher le nbr d'élément par categorie

--- script.js
+++ script.js
@@ -44,7 +44,31 @@ video.addEventListener("mouseout", function() {
   playButton.style.visibility = "hidden";
 });
 
-// ** transformer le button play on pause inerhtml + add class hover  
-//  + 
-// ** Problème de flicker a corriger
+// ********************************* blog *******************
+
+function filterGridItems(category) {
+  // Sélectionne tous les éléments de la grille
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  let count = 0; // Initialise le compteur à 0
+
+  // Parcours chaque élément de la grille
+  gridItems.forEach(item => {
+    // Si l'élément a la catégorie correspondante
+    if (item.dataset.category === category || category === "all") {  
+      // Affiche l'élément
+      item.style.display = 'block';
+      count++; // Incrémente le compteur
+    } else {
+      // Sinon masque l'élément
+      item.style.display = 'none';
+    }
+  });
+
+  // Met à jour le nombre d'éléments affichés dans le bouton
+  const button = document.querySelector(`a[href="#blog"][onclick="filterGridItems('${category}')"]`);
+  button.querySelector('input').value = `${category} (${count})`;
+}
+
+
 
